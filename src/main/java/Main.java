6eaa9e10a21b57afafc8274c472bc6dc65a72c1e
BogdanShulga <bogdan.shulga.90@gmail.com>
2019-06-29 14:47:55 +0300

    dev: added range from 24565 to 35789 to compute time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,36 @@
 package main.java;
 
-import main.java.classes.BanknoteKeeper;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         BanknoteKeeper banknoteKeeper = new BanknoteKeeper();
 
-        long start = System.currentTimeMillis();
-        System.out.println(banknoteKeeper.withdrawCash(2335424524242695565.6465461565465465465456));
-        long end = System.currentTimeMillis();
+        double n = 0;
+        boolean stop = false;
 
-        System.out.print("Execution time is " + (end - start) / 1000d + " seconds");
+        Scanner scanner = new Scanner(System.in);
+        while (!stop){
+            System.out.println("Please enter the amount of your money:");
+            try {
+                n = scanner.nextDouble();
+            } catch (Exception ex) {
+                System.out.println("You entered incorrect data!");
+                scanner.nextLine();
+                continue;
+            }
+            stop = true;
+        }
+
+        System.out.println("Your number of bills: ");
+        if (n > 24564 && n < 35790) {
+            long start = System.currentTimeMillis();
+            System.out.println(banknoteKeeper.withdrawCash(n));
+            long end = System.currentTimeMillis();
+            System.out.print("Execution time is " + (end - start) / 1000d + " seconds");
+        } else {
+            System.out.println(banknoteKeeper.withdrawCash(n));
+        }
     }
 }
